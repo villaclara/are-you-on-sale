9cac3ec20baf7999773432f8d10c530356eaaaec
vilalclara <34722004+villaclara@users.noreply.github.com>
2024-09-10 02:00:43 +0300

    wip regex from rz

---
+++ src/lib/Core/Services/ShopServices/RZ_Collector.cs
@@ -0,0 +1,62 @@
+﻿using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Core.Services.ShopServices;
+
+public static class RZ_Collector
+{
+	private static string _rztkApiLinkEmpty = @"https://rozetka.com.ua/api/product-api/v4/goods/get-main?goodsId=";
+
+	public static ProductBase GetProductBaseFromShop(string url)
+	{
+		var productId = GetIdFromLink(url);
+
+		using var client = new HttpClient();
+		var response = client.GetStringAsync(_rztkApiLinkEmpty + productId).Result;
+
+		if (response is null)
+		{
+			throw new Exception();
+		}
+
+		throw new Exception();
+
+	}
+
+
+	public static string GetIdFromLink(string url)
+	{
+		// URL TEMPLATE
+		// https://rozetka.com.ua/ua/steelseries_62513/p179706829/
+		var indexP = url.LastIndexOf('p');
+		string str = url.Substring(indexP + 1); // 179706829/
+		if(str.Last() == '/')
+		{
+			str = str.Remove(str.Length - 1);
+		}
+		return str;
+	}
+
+
+	public static string GetWithRegex(string source, string pattern)
+	{
+		var strs = Regex.Match(source, pattern);
+		return strs.Value;
+	}
+
+	// REGEX for title
+	// (\.*)(""title"": "".*"",""price"")
+
+	// REGEX for price
+	// (\.*)(""price"": ""[1-9]*"")
+
+	// REGEX for old price
+	// (\.*)(""old_price"": ""[1-9]*"")
+	// Expected 
+	// "old_price": "559"
+}
---
+++ src/tests/CoreTests/ShopServiceTests/GetProductFromLinkTests.cs
@@ -0,0 +1,91 @@
+﻿using Core.Services.ShopServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CoreTests.ShopServiceTests;
+
+public class GetProductFromLinkTests
+{
+
+	[Fact]
+	public void GetProductFromRZTKLink_ReturnProductIdAsString0()
+	{
+		// Arrange 
+		string rztkLink = @"https://rozetka.com.ua/ua/steelseries_62513/p179706829/";
+		string productId = "179706829";
+
+		// Act
+		var result = RZ_Collector.GetIdFromLink(rztkLink);
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.Equal(productId, result);
+	}
+
+	[Fact]
+	public void GetProductFromRZTKLink_ReturnProductIdAsString1()
+	{
+		// Arrange 
+		string rztkLink = @"https://rozetka.com.ua/ua/steelseries_62513/p387542565";
+		string productId = "387542565";
+
+		// Act
+		var result = RZ_Collector.GetIdFromLink(rztkLink);
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.Equal(productId, result);
+	}
+
+	[Fact]
+	public void GetPriceWithRegexRZ_ReturnString()
+	{
+		// Arrange 
+		string source = @"{""data"": {""id"": 387542565,""title"": ""Комплект 2 шт. акумуляторні батарейки Smartoools AA 2600 mah + зарядка type-C"",""price"": ""499"",""old_price"": ""559"",""price_pcs"": ""11.97"",""min_month_price"": ""0"",";
+		string pattern = @"(\.*)(""price"": ""[1-9]*"")";
+		string expected = @"""price"": ""499""";
+
+		// Act
+		var result = RZ_Collector.GetWithRegex(source, pattern);
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.Equal(expected, result);
+	}
+
+	[Fact]
+	public void GetTitleWithRegexRZ_ReturnString()
+	{
+		// Arrange 
+		string source = @"{""data"": {""id"": 387542565,""title"": ""Комплект 2 шт. акумуляторні батарейки Smartoools AA 2600 mah + зарядка type-C"",""price"": ""499"",""old_price"": ""559"",""price_pcs"": ""11.97"",""min_month_price"": ""0"",";
+		string pattern = @"(\.*)(""title"": "".*"",""price"")";
+		string expected = @"""title"": ""Комплект 2 шт. акумуляторні батарейки Smartoools AA 2600 mah + зарядка type-C"",""price""";
+
+		// Act
+		var result = RZ_Collector.GetWithRegex(source, pattern);
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.Equal(expected, result);
+	}
+
+	[Fact]
+	public void GetOldPriceWithRegexRZ_ReturnString()
+	{
+		// Arrange 
+		string source = @"{""data"": {""id"": 387542565,""title"": ""Комплект 2 шт. акумуляторні батарейки Smartoools AA 2600 mah + зарядка type-C"",""price"": ""499"",""old_price"": ""559"",""price_pcs"": ""11.97"",""min_month_price"": ""0"",";
+		string pattern = @"(\.*)(""old_price"": ""[1-9]*"")";
+		string expected = @"""old_price"": ""559""";
+
+		// Act
+		var result = RZ_Collector.GetWithRegex(source, pattern);
+
+		// Assert
+		Assert.NotNull(result);
+		Assert.Equal(expected, result);
+	}
+}
